45d1f7aaacb1cdbc0eb643c5cbdab2e11d191ab4
origotdaskill <139981840+origotdaskill@users.noreply.github.com>
2024-09-25 14:55:27 +0100

    shopSpot json add

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://shop-spot-backend.vercel.app/",
+    origin: "https://shop-spot-backend.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
