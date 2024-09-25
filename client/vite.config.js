6defc2f9ef3cf3ba502bfbefeda82b7e110c4124
origotdaskill <139981840+origotdaskill@users.noreply.github.com>
2024-09-25 18:21:50 +0100

    fix 404 not found

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Set the base URL
   plugins: [react()],
   resolve: {
     alias: {
