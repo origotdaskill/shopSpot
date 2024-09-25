33c7c8edfc794af922bb18a1ae45875467cd1665
origotdaskill <139981840+origotdaskill@users.noreply.github.com>
2024-09-25 15:04:47 +0100

    shopSpot render init

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://shop-spot-backend.vercel.app",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
